25d7b6a9f595dd8212eb7c931f2c0eecd8e6323b
Ajay Thakur <ajaythk.94@gmail.com>
2024-01-07 11:04:51 +0545

    config dict added

---
+++ config_dict.py
@@ -0,0 +1,17 @@
+from pydantic import ConfigDict, BaseModel, Field
+
+
+class Person(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)  # model_config helps to pass arguments by alias or column_name
+
+    first_name: str | None = Field(default=None, alias="firstname")
+    last_name: str = Field(alias="lastName")
+
+
+data = {
+    'first_name': 'John', 'last_name': 'Smith'
+}
+
+p = Person(first_name="John", lastName="Smith")
+print(p.model_dump())
+print(p.model_validate(data))
